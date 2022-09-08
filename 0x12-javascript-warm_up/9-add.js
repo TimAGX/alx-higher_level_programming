3c22606707991d08d77875d4991287991c58c9f7
TimAGX <ayomidetimothy28@gmail.com>
2022-09-08 09:51:57 -0700

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,6 +3,7 @@ const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
 function add (a, b){
   let addition = a + b;
-    console.log(addition);
+    return addition;
+      console.log(addition);
 }
 add(a + b);
