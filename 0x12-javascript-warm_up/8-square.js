3aeee07eea1e4c03a57d755732c5e88d21f9879f
TimAGX <ayomidetimothy28@gmail.com>
2022-09-08 09:40:05 -0700

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+let i = parseInt(process.argv[2]);
+if (Number.isNaN(i)) {
+  console.log('Missing size');
+} else {
+  while (i < i) {
+    console.log('X');
+    i--;
+     while(j < i){
+       console.log('X');
+       j--;
+    }
+  }
+}
